1610b7f04222d239617f08c539e4cbe7f66f310f
Alexey Khachaturyan <devcustrom@gmail.com>
2022-03-24 10:48:23 +0300

    Доработка админки

--- src/scripts/admin.js
+++ src/scripts/admin.js
@@ -1,5 +1,5 @@
 import CMS from 'netlify-cms'
-import { repoName } from '/src/theme/admin.js'
+import { repoName, alias } from '/src/theme/admin.js'
 
 export default new class  {
   constructor() {
@@ -7,6 +7,111 @@ export default new class  {
   }
 
   handler() {
+	const contacts = {
+		"name": "index",
+		"label": "Контактная информация",
+		"file": "/data/contacts.json",
+		"widget" : "list",
+		"editor": {
+			"preview": false,
+		},
+		"fields": [
+			{
+				"label": "Телефон",
+				"name": "phone",
+				"widget": "string"
+			},
+			{
+				"label": "Почта",
+				"name": "mail",
+				"widget": "string"
+			},
+			{
+				"label": "WhatsApp",
+				"name": "wa",
+				"widget": "string"
+			},
+			{
+				"label": "Адрес",
+				"name": "address",
+				"widget": "string"
+			},
+			{
+				"label": "Карта",
+				"name": "mapLink",
+				"widget": "string"
+			}
+		]
+	}
+	const settings = {
+		"name": "menu",
+		"label": "Настройки сайта",
+		"file": "/data/site.json",
+		"widget" : "list",
+		"editor": {
+			"preview": false,
+		},
+		"fields": [
+			{
+				"name": "name",
+				"label": "Название",
+				"widget": "string"
+			},
+			{
+				"name": "logoUrl",
+				"label": "Логотип",
+				"widget": "image"
+			},
+			{
+				"name": "faviconUrl",
+				"label": "Фавиконка",
+				"widget": "image"
+			},
+			{
+				"name": "department",
+				"label": "Отдел",
+				"widget": "string"
+			},
+			{
+				"name": "wmid",
+				"label": "WMID",
+				"widget": "string"
+			},
+			{
+				"name": "city",
+				"label": "Город",
+				"widget": "string"
+			},
+			{
+				"name": "api",
+				"label": "Ссылка на API",
+				"widget": "string"
+			},
+			{
+				"name": "credeoApi",
+				"label": "Credeo API?",
+				"widget": "boolean",
+				"default": true
+			},
+			{
+				"name": "source_id",
+				"label": "Источник",
+				"widget": "string"
+			},
+			{
+				"name": "broker_id",
+				"label": "Брокер",
+				"widget": "string"
+			}
+		]
+	}
+	let data
+	if(alias) {
+		data = [contacts, settings, alias]
+	} else {
+		data = [contacts, settings]
+	}
+
 	const config = {
 		"backend": {
 			name: "github",
@@ -20,41 +125,7 @@ export default new class  {
 			{
 				"label": 'Данные',
 				"name": 'lists',
-				"files": [
-					{
-						"name": "index",
-						"label": "Контактная информация",
-						"file": "/data/contacts.json",
-						"widget" : "list",
-						"fields": [
-							{
-								"label": "Телефон",
-								"name": "phone",
-								"widget": "string"
-							},
-							{
-								"label": "Почта",
-								"name": "mail",
-								"widget": "string"
-							},
-							{
-								"label": "WhatsApp",
-								"name": "wa",
-								"widget": "string"
-							},
-							{
-								"label": "Адрес",
-								"name": "address",
-								"widget": "string"
-							},
-							{
-								"label": "Карта",
-								"name": "mapLink",
-								"widget": "string"
-							}
-						]
-					}
-				]
+				"files": data
 			}
 		]
 	}
