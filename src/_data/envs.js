ed9f938e754c0f1285c6c5a2134196bf1b3eb6d4
credeoseo <81287389+credeoseo@users.noreply.github.com>
2023-04-12 16:13:10 +0300

    Update envs.js

--- src/_data/envs.js
+++ src/_data/envs.js
@@ -2,6 +2,7 @@ module.exports = function() {
 	return {
 		contacts: require("../data/contacts.json"),
 		menu: require("../data/menu.json"),
-		site: require("../data/site.json")
+		site: require("../data/site.json"),
+		noIndex: require('../data/noIndex.json')
 	};
 };
